a6482920f89e517c0cc7cdd659731b57a316df2f
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-04-24 08:18:19 -0500

    install ejs template engine

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
-import { createServer } from 'node:http';
+import express from 'express'
 import dotenv from 'dotenv';
 dotenv.config();
+import path from 'path' // import path to to use path.join of ejs engine below
+import { fileURLToPath } from 'url';
 
-const hostname = '127.0.0.1';
+ 
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// These two lines recreate __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// config template engine
+app.set('views' , path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// use template engine with template express, we are going to use EJS engine
+// install ejs with command npm instal --save-exact ejs@3.1.8
+//khai bao route below
+app.get('/', (req,res)=>{
+  res.send('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/example', (req,res)=>{
+  res.render('sample.ejs') // tạo view động, dùng render
+})
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`)
+})
 
