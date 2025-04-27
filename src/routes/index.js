63d0319d0f88ed3c010447c36cb908e327ba5c90
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-04-26 23:25:42 -0500

    working woth route files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,14 @@
 // Combines all the route files
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+
+router.get('/example', (req,res)=>{
+    res.render('sample.ejs') // tạo view động, dùng render
+})
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import userRoute from './routes/index.js'
 dotenv.config();
 import path from 'path' // import path to to use path.join of ejs engine below
 import { fileURLToPath } from 'url';
@@ -19,13 +20,17 @@ app.set('view engine', 'ejs')
 // use template engine with template express, we are going to use EJS engine
 // install ejs with command npm instal --save-exact ejs@3.1.8
 //khai bao route below
-app.get('/', (req,res)=>{
-  res.send('Hello World');
-});
+// app.get('/', (req,res)=>{
+//   res.send('Hello World');
+// });
 
-app.get('/example', (req,res)=>{
-  res.render('sample.ejs') // tạo view động, dùng render
-})
+// app.get('/example', (req,res)=>{
+//   res.render('sample.ejs') // tạo view động, dùng render
+// })
+
+app.use('/', userRoute);
+app.use('/example', userRoute);
+
 app.listen(port, () => {
   console.log(`Express server running at http://localhost:${port}`)
 })
