8c622eb7aa47b4433fa75ffc1340e35eacd4ce5c
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-04-28 09:36:46 -0500

    WIP: APi key, baseURL in models file

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,1 +1,20 @@
 //database schema( example with mongoose)
+const fetch = require('node-fetch');
+const API_KEY = process.env.API_KEY;
+const BASE_URL = process.env.BASE_URL;
+
+export const Get_Weather_Data = async (city) =>{
+    const API_URL = `${BASE_URL}?q=${city}&appid=${API_KEY}`;
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        throw error;
+    }
+}
+
